cba529195cc29a080679c393dff0b9e8bac25d09
dfrkp <github.com@mhhs.de>
2017-10-03 14:17:36 +0200

    re-fix stupid typo in org-iswitchb command

--- init.d/i-org.el
+++ init.d/i-org.el
@@ -8,7 +8,7 @@
 	     :bind (("C-c l" . org-store-link)
 		    ("C-c c" . org-capture)
 		    ("C-c a" . org-agenda)
-		    ("C-c b" . orb-ido-switchb))
+		    ("C-c b" . org-iswitchb))
 	     :config
 	     (setq org-directory (concat dropbox-directory "org/"))
 	     (add-to-list 'auto-mode-alist '("\\.org\\'" . org-mode))

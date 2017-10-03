7d157319a1d52eda775ba37f9996bbe05aa73a5f
dfrkp <github.com@mhhs.de>
2017-10-03 08:14:37 +0200

    switch to rjsx-mode for jsx
    actually works as it should...

--- init.d/i-web.el
+++ init.d/i-web.el
@@ -4,19 +4,16 @@
 ;;; Code:
 (require 'req-package)
 
-(req-package web-mode
+(req-package web-mode)
+
+(req-package rjsx-mode
   :require flycheck
   :config
-  (add-to-list 'auto-mode-alist '("\\.jsx?$" . web-mode))
+  (add-to-list 'auto-mode-alist '("\\.jsx?$" . rjsx-mode))
   (setq-default flycheck-disabled-checkers
   (append flycheck-disabled-checkers
     '(javascript-jshint)))
-  (flycheck-add-mode 'javascript-eslint 'web-mode)
-  (defadvice web-mode-highlight-part (around tweak-jsx activate)
-    (if (equal web-mode-content-type "jsx")
-	(let ((web-mode-enable-part-face nil))
-	  ad-do-it)
-      ad-do-it))
+  (flycheck-add-mode 'javascript-eslint 'rjsx-mode)
   )
 
 (req-package json-mode)

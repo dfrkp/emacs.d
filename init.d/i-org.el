c78551701a0023e30bf7abd71ba21a0dc86b23d3
dfrkp <github.com@mhhs.de>
2018-05-05 08:45:48 +0200

    configure org-export-backends

--- init.d/i-org.el
+++ init.d/i-org.el
@@ -45,25 +45,24 @@
   (setq org-outline-path-complete-in-steps nil)         ; Refile in a single go
   (setq org-refile-use-outline-path t)                  ; Show full paths for refiling
   (setq initial-major-mode 'org-mode)
-  (setq bookmark-default-file (concat org-directory "bookmarks.bmk")))
+  (setq bookmark-default-file (concat org-directory "bookmarks.bmk"))
+  (setq org-export-backends
+    '(ascii beamer html latex md odt confluence koma-letter)))
 
 (req-package org-bullets
   :ensure t
-  
   :require org
   :config
   (add-hook 'org-mode-hook 'org-bullets-mode))
 
 (req-package plantuml-mode
   :ensure t
-  
   :config
   (add-to-list
    'org-src-lang-modes '("plantuml" . plantuml)))
 
 (req-package htmlize
   :ensure t)
-  
 
 (provide 'i-org)
 ;;; i-org.el ends here

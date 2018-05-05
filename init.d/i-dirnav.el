3257726303e1085d47eeec45099da5bb0143d8d3
dfrkp <github.com@mhhs.de>
2018-05-05 08:44:54 +0200

    strip dirnav down to essentials
    sunrise is no longer available, alternative to be found

--- init.d/i-dirnav.el
+++ init.d/i-dirnav.el
@@ -4,22 +4,7 @@
 ;;; Code:
 (require 'req-package)
 
-(req-package dired
-  :ensure t
-  :config
-  (setq dired-listing-switches "-alXGh --group-directories-first")
-  (setq dired-omit-files
-	(concat dired-omit-files "\\|^\\..+$")))
-
-;; (req-package sunrise-commander
-;;   :config
-;;   (setq sr-autoload-extensions nil)
-;;   (setq sr-show-file-attributes nil)
-;;   (setq sr-confirm-kill-viewer nil)
-;;   (setq sr-show-hidden-files nil)
-;;   (setq sr-listing-switches "-alXGh --group-directories-first")
-;;   )
-
+(setq dired-listing-switches "-alXGh --group-directories-first")
 
 (provide 'i-dirnav)
 ;;; i-dirnav.el ends here

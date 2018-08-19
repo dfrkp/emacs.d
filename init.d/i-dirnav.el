39c5bfd1beba7113237bfd03099e02a928371dfe
dfrkp <github.com@mhhs.de>
2018-08-19 12:11:25 +0200

    change C-x s to sunrise rather than sunrise-cd

--- init.d/i-dirnav.el
+++ init.d/i-dirnav.el
@@ -6,7 +6,7 @@
 
 (req-package sunrise-commander
   :el-get t
-  :bind (("C-x s" . sunrise-cd))
+  :bind (("C-x s" . sunrise))
   :config
   (setq sr-autoload-extensions nil)
   (setq sr-show-file-attributes nil)

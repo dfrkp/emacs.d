9b08f0c323de27513725e11fc9ed1ab9547f9ec0
dfrkp <github.com@mhhs.de>
2018-09-07 16:36:46 +0200

    add w32-browser to open files w/ default app on windows

--- init.d/i-dirnav.el
+++ init.d/i-dirnav.el
@@ -12,10 +12,12 @@
   (setq sr-show-file-attributes nil)
   (setq sr-confirm-kill-viewer nil)
   (setq sr-show-hidden-files nil)
-  (setq sr-listing-switches "-alXGh --group-directories-first")
+  (setq sr-listing-switches "--time-style=locale --group-directories-first -alDhgG")
   )
 
 (setq dired-listing-switches "-alXGh --group-directories-first")
+
+(req-package w32-browser)
 
 (provide 'i-dirnav)
 ;;; i-dirnav.el ends here

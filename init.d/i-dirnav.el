5848f735e3b5e374eddc48c81132b465f552294d
dfrkp <github.com@mhhs.de>
2019-01-03 17:35:33 +0100

    improve dired config and windows ls support

--- init.d/i-dirnav.el
+++ init.d/i-dirnav.el
@@ -4,10 +4,12 @@
 ;;; Code:
 (require 'req-package)
 
-(setq dired-listing-switches "-aBhl --group-directories-first")
+(setq dired-listing-switches "-ABhlD --group-directories-first")
 
 (if (string-equal system-type "windows-nt")
     (progn
+      (setq ls-lisp-use-insert-directory-program t)      ;; use external ls
+      (setq insert-directory-program "c:/cygwin64/bin/ls.exe") ;; ls program name
       (req-package w32-browser
 	:config
 	(setq dired-load-hook

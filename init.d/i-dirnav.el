8e6a77e53cd58b0649b448662fb5492bbeed7d26
dfrkp <github.com@mhhs.de>
2019-06-14 11:05:48 +0200

    set proper dired startup defaults

--- init.d/i-dirnav.el
+++ init.d/i-dirnav.el
@@ -5,7 +5,15 @@
 (require 'req-package)
 
 (setq dired-listing-switches "-ABhlD --group-directories-first")
-(setq dired-dwim-target t)
+(setq dired-dwim-target t) ;; copy to other window
+
+(require 'dired-x)
+(setq-default dired-omit-files-p t) ; Buffer-local variable
+(setq dired-omit-files (concat dired-omit-files "\\|^\\..+$"))
+
+(add-hook 'dired-mode-hook
+      (lambda ()
+        (dired-hide-details-mode)))
 
 (if (string-equal system-type "windows-nt")
     (progn
@@ -13,7 +21,7 @@
       (setq insert-directory-program "c:/cygwin64/bin/ls.exe") ;; ls program name
       (req-package w32-browser
 	:config
-	(setq dired-load-hook
+	(add-hook dired-load-hook
 	      (lambda
 		(&rest ignore)
 		(define-key

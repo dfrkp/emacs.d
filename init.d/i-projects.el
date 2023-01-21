2ffc73215e2f07f15eca175f853afe334e44764c
Martin Schulze <developer@mhhs.de>
2023-01-21 16:22:22 +0100

    feat(projectile): use ag and ignore module dirs

--- init.d/i-projects.el
+++ init.d/i-projects.el
@@ -12,7 +12,12 @@
 	'(:eval (format " Projectile[%s]"
                         (projectile-project-name))))
   (projectile-mode +1)
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
+  (add-to-list 'projectile-globally-ignored-directories ".external_modules")
   (define-key projectile-mode-map (kbd "C-c p") 'projectile-command-map))
+
+(req-package ag
+  :ensure t)
 
 (provide 'i-projects)
 ;;; i-projects.el ends here

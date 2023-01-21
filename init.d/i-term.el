d8ff2e70ce1948d26b7345dc5bb60ad4e78620fb
Martin Schulze <developer@mhhs.de>
2023-01-21 16:23:12 +0100

    feat(term): switch to vterm

--- init.d/i-term.el
+++ init.d/i-term.el
@@ -4,21 +4,8 @@
 ;;; Code:
 (require 'req-package)
 
-(req-package sane-term
-  :ensure t
-  :bind (("C-x t" . sane-term)
-	 ("C-x T" . sane-term-create))
-  :config
-  (defun my-terminal-visible-bell ()
-    "A friendlier visual bell effect."
-    (invert-face 'mode-line)
-    (run-with-timer 0.1 nil 'invert-face 'mode-line))
-
-  (setq visible-bell nil
-	ring-bell-function 'my-terminal-visible-bell)
-
-  (add-hook 'term-mode-hook (lambda()
-			      (setq yas-dont-activate t))))
+(req-package vterm
+  :ensure t)
 
 (provide 'i-term)
 ;;; i-term.el ends here

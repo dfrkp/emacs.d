7a74c8e3497908545ea9775cc67687aae934c228
Martin Schulze <developer@mhhs.de>
2023-01-21 16:22:53 +0100

    feat(python): add python-black and code-cells (notebooks)

--- init.d/i-python.el
+++ init.d/i-python.el
@@ -7,15 +7,25 @@
 (req-package elpy
   :ensure t
   :init
-  (add-to-list 'auto-mode-alist '("\\.py$" . python-mode))
-  :custom
-  (elpy-rpc-backend "jedi"))
+  (elpy-enable)
+  :config
+  (setq elpy-rpc-python-command "python3"))
 
 (req-package python
   :ensure t
   :config
   (setq python-indent-offset 4)
   )
+
+(req-package python-black
+  :ensure t
+  :after python
+  :hook (python-mode . python-black-on-save-mode-enable-dwim))
+
+(req-package code-cells
+  :ensure t
+  :after python
+  :hook (python-mode . code-cells-mode-maybe))
 
 (provide 'i-python)
 ;;; i-python.el ends here
